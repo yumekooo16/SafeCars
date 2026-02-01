d797361113d6422418bd0e9d311ebdb1bbd76e01
Wyatt <wyatt.charleston16@gmail.com>
2026-02-01 22:22:05 +0100

    update commit

--- app/page.js
+++ app/page.js
@@ -153,16 +153,6 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-
-        {/* Scroll indicator */}
-        <div className="mt-20 absolute bottom-10 left-1/2 -translate-x-1/2 z-20 animate-bounce">
-          <div className="flex flex-col items-center gap-2 text-white/70">
-            <span className="text-xs tracking-widest uppercase">Défiler</span>
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-            </svg>
-          </div>
-        </div>
       </section>
 
       {/* Section Galerie de véhicules */}
